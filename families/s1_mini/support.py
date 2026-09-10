c11f14b284cf9eb4268a2480bf14220240ca3dd6
AbishekCoder1 <abisheknamachivaym@gmail.com>
2026-09-10 12:15:59 +0000

    chore: add missing SPDX license headers to support.py
    Signed-off-by: AbishekCoder1 <abisheknamachivaym@gmail.com>

--- families/s1_mini/support.py
+++ families/s1_mini/support.py
@@ -1,4 +1,7 @@
-"""Family-owned model and task support for s1_mini (S1-mini by Superwhisper)."""
+# SPDX-FileCopyrightText: Copyright (c) 2026 NVIDIA CORPORATION & AFFILIATES. All rights reserved.
+# SPDX-License-Identifier: Apache-2.0
+
+# """Family-owned model and task support for s1_mini (S1-mini by Superwhisper)."""
 
 from tensorrt_model_connect.model_support import family_support
 
